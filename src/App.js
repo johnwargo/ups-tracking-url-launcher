58fe980bd207932190fecdd4aaa4c27210b238c1
John M. Wargo <johnwargo@users.noreply.github.com>
2022-01-05 18:54:07 -0500

    completed first version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      trackingNumber: '',
+      trackingURL: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleChange(event) {
+    this.setState({ trackingNumber: event.target.value });
+  }
+
+  handleClick(event) {
+    window.open(`https://www.ups.com/track?loc=en_US&requester=QUIC&tracknum=${this.state.trackingNumber}/trackdetails`);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="flex items-center justify-between flex-wrap bg-gray-500 p-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <span className="font-semibold text-xl tracking-tight">UPS Tracking URL Launcher</span>
+          </div>
+        </nav>
+        <div className="container mx-auto px-5 py-5">
+          <label className='font-bold'>
+            UPS Tracking Number:&nbsp;
+            <input
+              className="px-4 py-3 w-80 rounded"
+              name="trackingNumber"
+              type="text"
+              value={this.state.trackingNumber}
+              onChange={this.handleChange}
+              required />
+          </label>
+          <br />
+          <button
+            hidden={this.state.trackingNumber.length === 0}
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-5 border border-blue-700 rounded"
+            onClick={this.handleClick}>
+            Launch Tracking URL
+          </button>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
