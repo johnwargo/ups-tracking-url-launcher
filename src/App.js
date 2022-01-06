dc4731c8150e057eb9ff7d62bab84c0fb3768e3d
John M. Wargo <johnwargo@users.noreply.github.com>
2022-01-06 06:53:25 -0500

    .

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   componentDidMount() {
     let dashes = '='.repeat(80);
     console.log(dashes);
-    console.log('Site: UPS Tracking URL Launcher');
+    console.log('UPS Tracking URL Launcher');
     console.log('Copyright John M. Wargo (john@johnwargo.com)');
     console.log(`Build: ${buildInfo.buildVersion} - ${buildDate.toString()}`);
     console.log('(build information generated using my `react-build-info` package: https://www.npmjs.com/package/react-build-info)');
